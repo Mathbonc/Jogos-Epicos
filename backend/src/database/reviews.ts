d0d4b99a80882fcf9643d29b36ba9b757ee3f531
Maria <mevm@cin.ufpe.br>
2023-07-26 17:10:43 -0300

    added my example database

---
+++ backend/src/database/reviews.ts
@@ -0,0 +1,20 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Days Gone',
+      rating: 5,
+      comment: 'Muito legal, amei matar as hordas!',
+      date: '2021-01-01',
+      categories: ['Zumbi', 'RPG'], 
+    },
+    {
+      id: 2,
+      name: 'God of War',
+      rating: 5,
+      comment: 'Jogo muito bom, recomendo!',
+      date: '2021-05-03',
+      categories: ['Ação', 'Aventura', 'RPG'],
+    }
+  ];
+
+export default reviews;
