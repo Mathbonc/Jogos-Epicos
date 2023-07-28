61a52e5ed6a40180298c47cc19d6fc98f3306b33
Maria <mevm@cin.ufpe.br>
2023-07-27 22:50:46 -0300

    feat(historico): added users in routes

--- backend/src/database/reviews.ts
+++ backend/src/database/reviews.ts
@@ -1,35 +1,59 @@
-const reviews = [
+type Review = {
+    id: number;
+    author_id: number;
+    name: string;
+    rating: number;
+    description: string;
+    date: string;
+    categories: string[];
+    comments: string[];
+    likes: number;
+  };
+
+const reviews: Review[] = [
     {
       id: 1,
+      author_id: 1,
       name: 'Days Gone',
       rating: 5,
-      comment: 'Muito legal, amei matar as hordas!',
+      description: 'Muito legal, amei matar as hordas!',
       date: '2021-01-01',
       categories: ['Zumbi', 'RPG'], 
+      comments: ['Muito bom mesmo!', 'Jogo muito bom, recomendo!'],
+      likes: 2,
     },
     {
       id: 2,
+      author_id: 1,
       name: 'God of War',
       rating: 5,
-      comment: 'Jogo muito bom, recomendo!',
+      description: 'Jogo muito bom, recomendo!',
       date: '2021-05-03',
       categories: ['Ação', 'Aventura', 'RPG'],
+      comments: ['Muito bom mesmo!', 'Jogo muito bom, recomendo!'],
+      likes: 5,
     },
     {
       id: 3,
+      author_id: 2,
       name: 'The Last of Us',
       rating: 4,
-      comment: 'Jogo muito bom, recomendo!',
+      description: 'Jogo muito bom, recomendo!',
       date: '2022-01-01',
       categories: ['Ação', 'Aventura', 'Zumbi', 'RPG'],
+      comments: ['Muito bom mesmo!', 'Jogo muito bom, recomendo!'],
+      likes: 3,
     },
     {
       id: 4,
+      author_id: 3,
       name: 'No Man\'s Sky',
       rating: 3,
-      comment: 'MineCraft no espaço, muito bom!',
+      description: 'MineCraft no espaço, muito bom!',
       date: '2020-08-01',
       categories: ['Aventura', 'RPG', 'Sobrevivência'],
+      comments: ['Muito bom mesmo!', 'Jogo muito bom, recomendo!'],
+      likes: 1,
     }
   ];
 
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,41 +4,113 @@ import TestController from '../controllers/test.controller';
 import TestService from '../services/test.service';
 
 import reviews from '../database/reviews';
+import users from '../database/users';
 
 const router = Router();
 const prefix = '/api';
 
-//Route to get all reviews
-router.get('/historico', (req: Request, res: Response) => {
-  res.json(reviews);
+//User with id 1 is logged in
+const logged_in_id = 1;
+
+//function to verify username
+function verifyUsername(username: string) {
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return null;
+  }
+  return user;
+}
+
+//function to verify review id
+function verifyReviewId(id: number) {
+  const review = reviews.find((review) => review.id === id);
+  if (!review) {
+    return null;
+  }
+  return review;
+}
+
+//Route to get all reviews of that user by username
+router.get('/user/:username/historico', (req: Request, res: Response) => {
+  //check user
+  const user = verifyUsername(req.params.username);
+  if (user === null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  const review = reviews.filter((review) => review.author_id === user.id);
+  res.json(review);
 });
 
+
 //Route to filter reviews by category
-router.get('/historico/category/:category', (req: Request, res: Response) => {
+router.get('/user/:username/historico/category/:category', (req: Request, res: Response) => {
+  //check user
+  const user = verifyUsername(req.params.username);
+  if (user === null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  //check category
   const category = req.params.category;
-  const review = reviews.filter((review) => review.categories.includes(category));
+  const review = reviews.filter((review) => review.categories.includes(category) && review.author_id === user.id);
+  if (review.length === 0) {
+    return res.status(404).json({ error: 'Any reviews with that category' });
+  }
+
   res.json(review);
 });
 
 //Route to get a review by id
-router.get('/historico/id/:id', (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const review = reviews.find((review) => review.id === id);
+router.get('/user/:username/historico/id/:id', (req: Request, res: Response) => {
+  //check user
+  const user = verifyUsername(req.params.username);
+  if (user === null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
 
-  if (!review) {
+  //check review id
+  const review = verifyReviewId(parseInt(req.params.id));
+  if (review === null) {
     return res.status(404).json({ error: 'Review not found' });
+  }
+
+  //check if review belongs to user
+  if (review.author_id !== user.id) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
   res.json(review);
 });
 
 //Route to update a review by id
-router.put('/historico/id/:id', (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const reviewIndex = reviews.findIndex((review) => review.id === id);
+//Only the author of the review can update it and only if the author is logged in
+router.put('/user/:username/historico/id/:id', (req: Request, res: Response) => {
+  //check user
+  const user = verifyUsername(req.params.username);
+  if (user === null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  //check review id
+  const review_to_edit = verifyReviewId(parseInt(req.params.id));
+  if (review_to_edit === null) {
+    return res.status(404).json({ error: 'Review not found' });
+  }
+
+  //check if review belongs to user
+  if (review_to_edit.author_id !== user.id) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const reviewIndex = reviews.findIndex((review_to_edit) => review_to_edit.id === parseInt(req.params.id));
 
   if (reviewIndex === -1) {
     return res.status(404).json({ error: 'Review not found' });
+  }
+
+  if (reviews[reviewIndex].author_id !== logged_in_id) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
   
   const review = reviews[reviewIndex];
@@ -48,12 +120,32 @@ router.put('/historico/id/:id', (req: Request, res: Response) => {
 });
 
 //Route to delete a review by id
-router.delete('/historico/id/:id', (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const reviewIndex = reviews.findIndex((review) => review.id === id);
+router.delete('/user/:username/historico/id/:id', (req: Request, res: Response) => {
+  //check user
+  const user = verifyUsername(req.params.username);
+  if (user === null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  //check review id
+  const review_to_delete = verifyReviewId(parseInt(req.params.id));
+  if (review_to_delete === null) {
+    return res.status(404).json({ error: 'Review not found' });
+  }
+
+  //check if review belongs to user
+  if (review_to_delete.author_id !== user.id) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const reviewIndex = reviews.findIndex((review_to_delete) => review_to_delete.id === parseInt(req.params.id));
 
   if (reviewIndex === -1) {
     return res.status(404).json({ error: 'Review not found' });
+  }
+
+  if (reviews[reviewIndex].author_id !== logged_in_id) {
+    return res.status(401).json({ error: 'Unauthorized' });
   }
 
   reviews.splice(reviewIndex, 1);
